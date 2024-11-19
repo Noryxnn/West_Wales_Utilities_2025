a3af626ea6714e1009aa84363296cb64e512f18e
c23069495 <sabata@cardiff.ac.uk>
2024-11-19 12:43:40 +0000

    Initial commit of scaffolded Spring Boot project.

---
+++ src/test/java/uk/ac/cf/spring/client_project/ClientProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.spring.client_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
