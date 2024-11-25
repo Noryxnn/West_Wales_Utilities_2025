903ecf51d8ae59a7e2110e74f9af489f91af4bb5
c23069495 <sabata@cardiff.ac.uk>
2024-11-25 22:57:58 +0000

    Solved the input validation errors.

--- src/main/java/uk/ac/cf/spring/client_project/request/RequestForm.java
+++ src/main/java/uk/ac/cf/spring/client_project/request/RequestForm.java
@@ -1,6 +1,8 @@
 package uk.ac.cf.spring.client_project.request;
 
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,13 +14,15 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class RequestForm {
     private Long requestId;
-    @NotNull(message = "User ID field cannot be empty.")
+    @NotNull(message = "User ID is required.")
+    @Min(value = 1, message = "User ID must be greater than 0.")
     private Long userId;
-    @NotNull (message = "Location ID field cannot be empty.")
+    @NotNull(message = "Location ID is required.")
+    @Min(value = 1, message = "Location ID must be greater than 0.")
     private Long locationId;
-    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate requestDate;
-    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Future (message = "Visit date must be in the future.")
     private LocalDate visitDate;
 
