ec92a4892e927c7b284bedf22243e4e7d755c292
c23064426 <zhaos32@cardiff.ac.uk>
2024-12-03 17:44:56 +0000

    implemented a feature enabling the viewing of pending requests (no bean errors this time) and it works for the admin to see the requests

--- src/main/java/uk/ac/cf/spring/client_project/request/Request.java
+++ src/main/java/uk/ac/cf/spring/client_project/request/Request.java
@@ -1,7 +1,5 @@
 package uk.ac.cf.spring.client_project.request;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -18,12 +16,17 @@ public class Request {
     private LocalDate visitEndDate;
 
 
-    public Request(Long requestId, @NotNull(message = "User ID is required.") Long userId, LocalDate requestDate, @Future(message = "Visit date must be in the future.") @NotNull(message = "Visit date is required.") LocalDate visitStartDate, LocalDate visitEndDate) { this(0L, 0L, 0L, null, null,null);
+    /*public Request(Long requestId, @NotNull(message = "User ID is required.") Long userId, LocalDate requestDate, @Future(message = "Visit date must be in the future.") @NotNull(message = "Visit date is required.") LocalDate visitStartDate, LocalDate visitEndDate) { this(0L, 0L, 0L, null, null,null);
+
+    }*/
+
+    public Request(Long aRequestId,Long aUserId,LocalDate aRequestDate,LocalDate aVisitStartDate,LocalDate aVisitEndDate) {
+        this(aRequestId,aUserId,0L,aRequestDate,aVisitStartDate,aVisitEndDate);
     }
 
-    public Request(long requestId, long userId, long locationId, LocalDate requestDate, LocalDate localDate, LocalDate localDate1) {
 
-    }
+
+
 
     public boolean isNew() {
         return this.requestId == 0;
