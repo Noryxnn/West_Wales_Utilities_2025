ad7b168afb9a7c00e978daa6eb853394765ed0bb
c23064426 <zhaos32@cardiff.ac.uk>
2024-12-06 00:59:20 +0000

    made 'changes' to AdminServiceImpl.java though it seems the same as branch 53

--- src/main/java/uk/ac/cf/spring/client_project/request/admin/AdminServiceImpl.java
+++ src/main/java/uk/ac/cf/spring/client_project/request/admin/AdminServiceImpl.java
@@ -6,19 +6,19 @@ import uk.ac.cf.spring.client_project.request.RequestRepository;
 
 import java.util.List;
 
-    @Service
-    public class AdminServiceImpl implements AdminService {
-        private RequestRepository requestRepository;
-        public AdminServiceImpl(RequestRepository requestRepository) {
+@Service
+public class AdminServiceImpl implements AdminService {
+    private RequestRepository requestRepository;
+    public AdminServiceImpl(RequestRepository requestRepository) {
 
-            this.requestRepository = requestRepository;
-        }
+        this.requestRepository = requestRepository;
+    }
 
 
-        @Override
-        public List<Request> findPendingRequests() {
-            // Implement logic to find pending requests
-            return requestRepository.getOpenRequests();  // Adjust according to your logic
-        }
+    @Override
+    public List<Request> findPendingRequests() {
+        // Implement logic to find pending requests
+        return requestRepository.getOpenRequests();  // Adjust according to your logic
+    }
 
 }
