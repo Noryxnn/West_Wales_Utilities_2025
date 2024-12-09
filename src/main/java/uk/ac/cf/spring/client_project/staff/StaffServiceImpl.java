7f28eadadd70c046c1357164d5b15ba6c6036272
c23064426 <zhaos32@cardiff.ac.uk>
2024-12-09 22:15:59 +0000

    made some changes to StaffServiceImpl.java

--- src/main/java/uk/ac/cf/spring/client_project/staff/StaffServiceImpl.java
+++ src/main/java/uk/ac/cf/spring/client_project/staff/StaffServiceImpl.java
@@ -1,5 +1,7 @@
 package uk.ac.cf.spring.client_project.staff;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.ac.cf.spring.client_project.request.Request;
