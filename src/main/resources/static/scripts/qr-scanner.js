2a67e081283e4c8bba98513505a6b863f275d1af
c23060249 <lydfordw@cardiff.ac.uk>
2024-12-11 01:02:52 +0000

    Improved styling for qr scanner and scan results

--- src/main/resources/static/scripts/qr-scanner.js
+++ src/main/resources/static/scripts/qr-scanner.js
@@ -13,6 +13,7 @@ async function startQrScanner() {
     const html5QrCode = new Html5Qrcode("qr-reader");
     const qrReaderResults = document.getElementById("qr-reader-results");
     qrReaderResults.innerHTML = "";
+    qrReaderResults.style.display = "none";
 
     let isProcessing = false;
 
@@ -38,21 +39,21 @@ async function startQrScanner() {
                 // Process server response
                 console.log('Server Response:', data);
                 if (data.includes('success')) {
-                    qrReaderResults.innerHTML = `<p style="color: green;">Check-in successful</p>`;
+                    qrReaderResults.innerHTML = `<p class="scan-status" style="color: green;">Check-in successful</p>`;
 
                 } else if (data.includes('denied')) {
-                    qrReaderResults.innerHTML = `<p style="color: red;">Check-in denied</p>`;
+                    qrReaderResults.innerHTML = `<p class="scan-status" style="color: red;">Check-in denied</p>`;
 
                 } else {
-                    qrReaderResults.innerHTML = `<p>Invalid QR code</p>`;
+                    qrReaderResults.innerHTML = `<p class="scan-status">Invalid QR code</p>`;
                 }
                 qrReaderResults.innerHTML += `<button class="btn-primary" id="scan-again-btn" onclick="startQrScanner()">Scan Again</button>`;
 
-
+                qrReaderResults.style.display = 'block';
             })
             .catch(error => {
                 console.error('Error sending data:', error);
-                qrReaderResults.innerHTML = `<p style="color: red;">Error sending data: ${error}</p>`;
+                qrReaderResults.innerHTML = `<p class="scan-status" style="color: red;">Error sending data: ${error}</p>`;
             })
             .finally(() => {
                 html5QrCode.stop();
