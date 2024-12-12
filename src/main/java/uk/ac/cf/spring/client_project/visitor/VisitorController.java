9734e428ef2419e4c406c11e0437fe139ed9c593
c23060249 <lydfordw@cardiff.ac.uk>
2024-12-12 06:56:39 +0000

    QR code finally gets user id from logged-in user

--- src/main/java/uk/ac/cf/spring/client_project/visitor/VisitorController.java
+++ src/main/java/uk/ac/cf/spring/client_project/visitor/VisitorController.java
@@ -7,19 +7,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import uk.ac.cf.spring.client_project.qrcode.QRCodeGenerator;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import uk.ac.cf.spring.client_project.user.UserRepository;
+
 @Controller
 public class VisitorController {
     private static final Logger logger = LoggerFactory.getLogger(VisitorController.class);
+    UserRepository userRepository;
+
+    public VisitorController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/dashboard")
     public String getDashboard() {
         return "visitor/visitor-dashboard";
     }
 
-    @GetMapping("/access")
+    @GetMapping("/check-in")
     public ModelAndView checkIn() {
         ModelAndView modelAndView = new ModelAndView("visitor/access");
         try {
-            String qrcode = QRCodeGenerator.getQRCode(800, 800);
+            // Get current user ID from security context
+            Long userId = userRepository.findByEmail(((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername())
+                    .map(user -> Long.valueOf(user.getUserId()))
+                    .orElseThrow(() -> new IllegalStateException("User ID not found"));
+
+
+            String qrcode = QRCodeGenerator.getQRCode(userId, 300, 300);
             logger.info("Successfully generated QR code for visitor");
 
             modelAndView.addObject("qrcode", qrcode);
